9416e9712b0bbff077ba1fe3591969a027f639f6
Carlos <carlosca@student.chalmers.se>
2023-09-20 13:27:35 +0200

    Co-authored-by: Andrii Demchenko <demch.andrew@gmail.com> Co-authored-by: Marcelo Santibáñez <ssheloors@users.noreply.github.com>

---
+++ client/src/firebaseConfig.js
@@ -0,0 +1,11 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export default {
+  apiKey: 'AIzaSyBiiV9ENBTmIwXvXhwJRDLzhvDY7weoO78',
+  authDomain: 'weplay-g26.firebaseapp.com',
+  projectId: 'weplay-g26',
+  storageBucket: 'weplay-g26.appspot.com',
+  messagingSenderId: '1026419486068',
+  appId: '1:1026419486068:web:17e9a13596fc4938af9021',
+  measurementId: 'G-Z43ENZ8H33'
+}
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,16 +1,23 @@
 <template>
   <div>
-    <b-jumbotron header="DIT342 Frontend" lead="Welcome to your DIT342 Frontend Vue.js App">
-      <b-button class="btn_message" variant="primary" v-on:click="getMessage()" >Get Message from Server</b-button>
-      <p>Message from the server:<br/>
-      {{ message }}</p>
-    </b-jumbotron>
+      <div id="firebaseui-auth-container"></div>
+      <div id="loader">Loading...</div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import { Api } from '@/Api'
+import { ref } from 'vue'
+import firebaseConfig from '../firebaseConfig'
+
+// v9 firebase
+import firebase from 'firebase/compat/app'
+
+import * as firebaseui from 'firebaseui'
+import 'firebaseui/dist/firebaseui.css'
+
+firebase.initializeApp(firebaseConfig)
 
 export default {
   name: 'home',
@@ -28,6 +35,53 @@ export default {
         .catch(error => {
           this.message = error
         })
+    }
+  },
+  setup() {
+    const user = ref(null)
+    const isSignedIn = ref(false)
+
+    const uiConfig = {
+      callbacks: {
+        signInSuccessWithAuthResult: function (authResult, redirectUrl) {
+          // User successfully signed in.
+          // Return type determines whether we continue the redirect automatically
+          // or whether we leave that to developer to handle.
+
+          user.value = authResult.user
+          console.log(user.value.displayName)
+          isSignedIn.value = true
+
+          return true
+        },
+        uiShown: function () {
+          // The widget is rendered.
+          // Hide the loader.
+          document.getElementById('loader').style.display = 'none'
+        }
+      },
+      // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
+      signInFlow: 'popup',
+      signInSuccessUrl: '/',
+      signInOptions: [
+        // Leave the lines as is for the providers you want to offer your users.
+        firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+        firebase.auth.EmailAuthProvider.PROVIDER_ID
+      ],
+      // Terms of service url.
+      tosUrl: '<your-tos-url>',
+      // Privacy policy url.
+      privacyPolicyUrl: '<your-privacy-policy-url>'
+    }
+
+    // Initialize the FirebaseUI Widget using Firebase.
+    const ui = new firebaseui.auth.AuthUI(firebase.auth())
+
+    ui.start('#firebaseui-auth-container', uiConfig)
+
+    return {
+      user,
+      isSignedIn
     }
   }
 }
