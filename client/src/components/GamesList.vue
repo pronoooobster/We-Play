807646cfc16b41c0acf455f244ae2eedb3bc2869
Marcelo <gussanma@student.gu.se>
2023-10-07 16:28:38 +0200

    Methods for getting the specific games from API

--- client/src/components/GamesList.vue
+++ client/src/components/GamesList.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h1>Game Elements</h1>
-    <p> {{ games }}</p>
+    <p> {{ gamesFromApi }}</p>
   </div>
 </template>
 
@@ -11,28 +11,45 @@ export default {
     name: 'GamesList',
     data() {
         return {
-            games: [],
+            gamesFromApi: [], //store games from API    
+            gamesFromFile: [], //store specified games in the external file
         }
     },
 
     mounted() { //request game list from RAWG API
-        // const apiKey = "4179ef8b1acb4bae99d6f2675731f8a3"
-        const options = {
-            method: 'GET',
-            url: 'https://api.rawg.io/api/games?key=4179ef8b1acb4bae99d6f2675731f8a3',
-            
-        };
-
-        axios
-            .request(options)
-            .then(response => {
-                this.games = this.games.concat(response.data.results);
-            
-            })
-            .catch(error => {
-                console.error('Error fetching game data:', error);
-            });
+        this.fetchGamesFromFile();
+        this.fetchGamesFromDatabase();
     },
+
+    methods: {
+        async fetchGamesFromFile(){ //get the games from the JSON file and put them on the array
+            try {
+                //TODO: change to the real file and figure out how to take it from the JSON
+                // const response = await fetch(require("@/assets/games.json")); 
+                // this.gamesFromFile = await response.json();
+                this.gamesFromFile = ["The Witcher 3: Wild Hunt", "The Elder Scrolls V: Skyrim", "Grand Theft Auto V", "The Legend of Zelda: Breath of the Wild", "Red Dead Redemption 2", "Mass Effect 2", "The Last of Us", "God of War", "The Witcher 3: Wild Hunt", "The Elder Scrolls V: Skyrim", "Grand Theft Auto V", "The Legend of Zelda: Breath of the Wild", "Red Dead Redemption 2", "Mass Effect 2", "The Last of Us", "God of War"];
+                console.log(this.gamesFromFile);
+
+            } catch (error) {
+                console.log(error);
+            }
+        },
+        async fetchGamesFromDatabase(){
+            try {
+                //loop through the array of games using as refference the names stated on the JSON file
+                for (const gameName of this.gamesFromFile){
+                    const response = await axios.get(`https://api.rawg.io/api/games?key=4179ef8b1acb4bae99d6f2675731f8a3&search=${gameName}`); //get the game from the API
+                    const data = response.data.results[0]; //get the first result
+                if (data){
+                    this.gamesFromApi.push(data); //add the game to the array
+                } else {console.log("Game not found")}
+            }            
+            } catch (error) {
+                console.log(error);
+            }
+
+        }
+    }
 }
 
 
