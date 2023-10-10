fc4ff59ea24f13779cc4fc7519181cefade543aa
Carlos <carlosca@student.chalmers.se>
2023-10-10 15:36:09 +0200

    feat. extracted the clan list to a separate component

---
+++ client/src/components/ClanList.vue
@@ -0,0 +1,66 @@
+<template>
+  <div>
+    <div class="card-1" mx-auto v-if="usersClans">
+      <p class="clans" v-for="(clan, index) in usersClans" :key="index">
+        {{ clan._id }}
+      </p>
+    </div>
+  </div>
+</template>
+
+
+<script>
+
+const axios = require('axios');
+import { ref, onMounted } from 'vue';
+
+export default {
+
+  props: ["id"],
+
+  setup(props) {
+    const usersClans = ref(null);
+
+    const fetchUserClans = () => {
+
+      axios.get(`http://localhost:3000/api/users/${props.id}/clans`)
+        .then(response => {
+
+          if (response.data) {
+            usersClans.value = response.data;
+          }
+          else {
+            console.error('Invalid or missing data in the response.');
+          }
+        })
+        .catch(error => {
+          console.error('Error fetching clan data: ' + error);
+        });
+    };
+
+    onMounted(() => {
+      fetchUserClans();
+    });
+
+    return {
+      usersClans,
+    }
+
+  }
+}
+
+</script>
+
+
+<style>
+.card-1 {
+  margin: 4%;
+  padding-bottom: 4%;
+  background-color: #fed665 !important;
+  border-radius: 30px;
+}
+
+.clans {
+  text-align: center;
+}
+</style>
