0f6d864ac1cfc6c0373b6442d8a56b352c8fe80d
Marcelo <gussanma@student.gu.se>
2023-10-17 17:31:28 +0200

    users section on specific clan page

--- client/src/views/ClanPage.vue
+++ client/src/views/ClanPage.vue
@@ -1,25 +1,39 @@
 <template>
-  <div>
+  <div id="background">
     <TopBar />
-  </div>
-  <div>
-    <div class="card-1" mx-auto v-if="clan && clan.name">
-      <h1 class="display-1"> {{ clan.name }}</h1>
-      <p>Size: {{ clan.size }}</p>
-      <div class="card-2" mx-auto>
-        <div v-if="clan.users.length > 0">
-          <h3> Members: </h3>
-          <p class="users" v-for="(user, index) in clan.users" :key="index">
-            {{ user.name }}
-          </p>
-        </div>
-        <div v-else>
-          The clan is empty
+    <div id="component" class="container-fluid py-3">
+      <div class="card-1" mx-auto v-if="clan && clan.name">
+        <div class="card-header">{{ clan.name }}</div>
+        <div class="card-body">
+          <p>Size: {{ clan.size }}</p>
+          <div class="card-2">
+            <div v-if="clan.users.length > 0">
+              <div class="card-header" style="font-size: 30px; color: #1F1F1F; margin-top: 2px">Users</div>
+              <div class="card-body">
+                <div class="users" v-for="(user, index) in clan.users" :key="index">
+                  <div class="row">
+                    <div class="col-2">
+                      <div class="profile-img" @click="redirect(user._id)">
+                        <img v-if="user.photoURL" :src="user.photoURL" alt="Profile Picture" class="profile-img">
+                        <img v-else src="../assets/default-profile.png" alt="Profile Picture" class="profile-img">
+                      </div>
+                    </div>
+                    <div class="col-8" style="margin-top: 30px;">
+                      <h4 @click="redirect(user._id)">{{ user.name }}</h4>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div v-else class="users">
+              The clan is empty
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-    <div v-else>
-      <p>Loading...</p>
+      <div v-else>
+        <p>Loading...</p>
+      </div>
     </div>
   </div>
 </template>
@@ -67,31 +81,72 @@ export default {
       clan,
     }
 
-  }
+  },
+  methods: {
+    redirect(_uid) {
+      this.$router.push(`/profile/${_uid}`);
+    },
+  } 
 }
 
 </script>
 
 <style scoped>
-* {
-  font-family: 'Martian Mono';
+.card-header{
+  font-family: 'Press Start 2p';
+  color: #FED665;
+  font-size: 80px;
+  /*make font smaller on phones */
+  @media (max-width: 600px) {
+    font-size: 40px;
+  }
+}
+.users{
+  font-size: 30px;
+      /* hover controls */
+  -webkit-transform: perspective(1px) translateZ(0);
+  transform: perspective(1px) translateZ(0);
+  -webkit-transition-duration: 0.3s;
+  transition-duration: 0.6s;
+  -webkit-transition-property: transform;
+  transition-property: transform;
+}
+.users:hover{
+  cursor: pointer;
+  -webkit-transform: scale(0.95);
+  transform: scale(0.95);
+}
+h4{
+    margin-top: 3%;
+
+}
+.profile-img{
+    border-radius: 30%;
+
 }
 
 .card-1 {
   margin: 4%;
   padding-bottom: 4%;
-  background-color: #fed665 !important;
+  background-color: #1F1F1F;
   border-radius: 30px;
 }
 
 .card-2 {
   margin: 7%;
-  padding: 3%;
-  background-color: rgb(203, 174, 250);
+  padding: 2%;
+  background-color: #F7D063;
   border-radius: 20px;
 }
 
 .users {
   text-align: left;
+  font-family: 'Martian Mono';
+}
+
+#background {
+  height: 100vh;
+  width: 100vw;
+  overflow: auto;
 }
 </style>
